efb5b0c1f513631d28fb7c04486ed87409d2372f
GabrielF-pr <gabrielalcantara2705@gmail.com>
2020-05-19 22:37:44 -0300

    Starting to implement Board and Piece

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -14,20 +14,18 @@ public class Position {
 		return row;
 	}
 
-	public void setRow(Integer row) {
-		this.row = row;
-	}
-
 	public Integer getColumn() {
 		return column;
 	}
 
-	public void setColumn(Integer column) {
-		this.column = column;
-	}
 	
 	@Override
 	public String toString() {
 		return row + ", " + column;
+	}
+	
+	public void setValues(Integer row, Integer column){
+		this.row = row;
+		this.column = column;
 	}
 }
